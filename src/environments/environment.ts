f2a11f8c5c63e735d036935fd680b5f27a5718e8
panobato <henjinavarro@gmail.com>
2026-07-24 17:03:15 -0700

    Adding Google Authenticator

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyAmJOyPkHENa9PIrQB_XC6WNkJL0GpGT-I",
+  authDomain: "friendlychat-c8877.firebaseapp.com",
+  projectId: "friendlychat-c8877",
+  storageBucket: "friendlychat-c8877.firebasestorage.app",
+  messagingSenderId: "719044826992",
+  appId: "1:719044826992:web:9f2675e460f8500409a1b5",
   },
 };
