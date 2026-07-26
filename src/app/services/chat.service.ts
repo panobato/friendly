a7644086bacc576073b2c5f5194b2f74afecf1ba
panobato <henjinavarro@gmail.com>
2026-07-25 22:21:01 -0700

    Show new chats in the UI

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -141,7 +141,10 @@ export class ChatService {
 
   // Loads chat messages history and listens for upcoming ones.
   loadMessages = () => {
-    return null as unknown;
+    // Create the query to load the last 12 messages and listen for new ones.
+    const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'desc'), limit(12));
+    // Start listening to the query.
+    return collectionData(recentMessagesQuery);
   };
 
   // Saves a new message containing an image in Firebase.
